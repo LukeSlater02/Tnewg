f24c47ddaa304f7cf19b7d4410b75b6052a84473
LukeSlater02 <lukeslater101@gmail.com>
2022-07-15 16:31:16 -0500

    working on card ui

--- client/src/components/battles/battleMap.js
+++ client/src/components/battles/battleMap.js
@@ -21,26 +21,11 @@ export const BattleMap = () => {
 
                 <div className="card">
                     <div className="cardHeroImage">
-                        <img src="img/wizardCard.png"></img>
+                        <img src="img/circleGray.png" className="circle"></img>
+                        <span className="cost">6</span>
+                        <img src="img/wizardCard2.png"></img>
                     </div>
-                    <div className="statsContainer">
-                        <div className="stats">
-                            <img src="img/cardDmg.png"></img> 
-                        </div>
-                        <div className="stats">
-                            <img src="img/cardArmor.png"></img> 
-                        </div>
-                        <div className="stats">
-                            <img src="img/cardHp.png"></img> 
-                        </div>
-                    </div>
-                    <h2>Dark Mage</h2>
-                </div>
-
-                <div className="card">
-                    <div className="cardHeroImage">
-                        <img src="img/pose1.png"></img>
-                    </div>
+                    <h2>The Arcanist</h2>
                     <div className="statsContainer">
                         <div className="stats">
                             <img src="img/cardDmg.png"></img>
@@ -52,7 +37,26 @@ export const BattleMap = () => {
                             <img src="img/cardHp.png"></img>
                         </div>
                     </div>
-                    <h2>Valiant Knight</h2>
+                </div>
+
+                <div className="card">
+                    <div className="cardHeroImage">
+                    <img src="img/circleGray.png" className="circle"></img>
+                        <span className="cost">3</span>
+                        <img src="img/skeletonCard.png"></img>
+                    </div>
+                    <h2>Skeleton</h2>
+                    <div className="statsContainer">
+                        <div className="stats">
+                            <img src="img/cardDmg.png"></img>
+                        </div>
+                        <div className="stats">
+                            <img src="img/cardArmor.png"></img>
+                        </div>
+                        <div className="stats">
+                            <img src="img/cardHp.png"></img>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
