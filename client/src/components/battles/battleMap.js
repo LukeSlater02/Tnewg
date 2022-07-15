aea3d2b8930e0a348b477fa1a817b958810301bf
LukeSlater02 <lukeslater101@gmail.com>
2022-07-15 15:44:12 -0500

    working on card ui

--- client/src/components/battles/battleMap.js
+++ client/src/components/battles/battleMap.js
@@ -23,36 +23,36 @@ export const BattleMap = () => {
                     <div className="cardHeroImage">
                         <img src="img/wizardCard.png"></img>
                     </div>
-                    <h2>Dark Mage</h2>
                     <div className="statsContainer">
                         <div className="stats">
-                            <img src="img/cardDmg.png"></img> ----------
+                            <img src="img/cardDmg.png"></img> 
                         </div>
                         <div className="stats">
-                            <img src="img/cardArmor.png"></img> ----------
+                            <img src="img/cardArmor.png"></img> 
                         </div>
                         <div className="stats">
-                            <img src="img/cardHp.png"></img> ----------
+                            <img src="img/cardHp.png"></img> 
                         </div>
                     </div>
+                    <h2>Dark Mage</h2>
                 </div>
 
                 <div className="card">
                     <div className="cardHeroImage">
                         <img src="img/pose1.png"></img>
                     </div>
-                    <h2>Valiant Knight</h2>
                     <div className="statsContainer">
                         <div className="stats">
-                            <img src="img/cardDmg.png"></img> ----------
+                            <img src="img/cardDmg.png"></img>
                         </div>
                         <div className="stats">
-                            <img src="img/cardArmor.png"></img> ----------
+                            <img src="img/cardArmor.png"></img>
                         </div>
                         <div className="stats">
-                            <img src="img/cardHp.png"></img> ----------
+                            <img src="img/cardHp.png"></img>
                         </div>
                     </div>
+                    <h2>Valiant Knight</h2>
                 </div>
             </div>
         </div>
